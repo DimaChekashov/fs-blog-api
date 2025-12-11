ef8ea185f5a01509be46437a0ef3ca7ae3009feb
DimaChekashov <foxdima99@gmail.com>
2025-12-12 01:31:21 +0300

    Remove ts-node-dev

--- src/server.ts
+++ src/server.ts
@@ -1,9 +1,14 @@
+import dotenv from "dotenv";
 import express, { type Request, type Response } from "express";
 
 const app = express();
 
-app.get("/", (req: Request, res: Response) => {
+dotenv.config();
+
+app.get("/hello", (req: Request, res: Response) => {
   res.send("<h1>Hello World!</h1>");
 });
 
-app.listen(8000);
+app.listen(process.env.SERVER_PORT, () => {
+  console.log(`Server running on http://localhost:${process.env.SERVER_PORT}`);
+});
