81fb39bf23c1d15c8e58cc0672c3ad17e4693132
DimaChekashov <foxdima99@gmail.com>
2025-12-23 01:17:52 +0300

    Prepare comments model and migration

---
+++ src/models/comment.model.ts
@@ -0,0 +1,25 @@
+import { z } from "zod";
+import { PostIdSchema } from "./post.model.ts";
+import { UserIdSchema } from "./user.model.ts";
+import { CreatedAt, UpdatedAt } from "./index.model.ts";
+
+export const CommentIdSchema = z.number().int().positive().brand<"PostId">();
+export type CommentId = z.infer<typeof CommentIdSchema>;
+
+export const MessageSchema = z.string().min(1, "Message is required").trim();
+
+export const CommentSchema = z.object({
+  id: CommentIdSchema,
+  message: MessageSchema,
+  postId: PostIdSchema,
+  userId: UserIdSchema,
+  createdAt: CreatedAt,
+  updatedAt: UpdatedAt,
+});
+export type Comment = z.infer<typeof CommentSchema>;
+
+export const CreateCommentSchema = z.object({
+  message: MessageSchema,
+  postId: PostIdSchema,
+});
+export type CreateCommentDto = z.infer<typeof CreateCommentSchema>;
